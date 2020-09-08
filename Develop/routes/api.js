e34cfe711d618f133fe7db7e1546451f81d61fd8
Alix Wawa <alixwawa@yahoo.com>
2020-09-08 08:04:26 -0500

    added module.exports to bottom of api route page and it works now

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,2 +1,15 @@
 const router = require("express").Router();
 const Workouts = require("../models/workouts.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+    console.log(body)
+    Workouts.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  module.exports = router;
